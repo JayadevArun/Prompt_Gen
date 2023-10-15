03c02b9c4e9ff93c9573ccc6eea54e6bc7caf7bd
JayadevArun <jayadevarun03@gmail.com>
2023-10-16 00:18:24 +0530

    approved sign-in authentication

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,9 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from 'next-auth/providers/google';
 
+import User from "@models/user";
+import { connectToDB } from "@utils/database";
+
 const handler=NextAuth({
     providers:[
         GoogleProvider({
@@ -9,14 +12,33 @@ const handler=NextAuth({
         })
     ],
     async session({session}){
-        
+        const sessionUser=await User.findOne({
+            email:session.user.email
+        })
+        session.user.id=sessionUser._id.toString();
+        return session;
     },
     async signIn({profile}){
         try{
+            await connectToDB();
 
+            const userExist=await User.findOne({
+                email:profile.email
+            });
+
+            if(!userExist){
+                await User.create({
+                    email:profile.email,
+                    username:profile.name.replace(" ","").toLowerCase(),
+                    image:profile.picture
+                })
+            }
+
+            return true;
         }
         catch(error){
-
+            console.log(error);
+            return false;
         }
     }
 })
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,17 +6,17 @@ import { useEffect,useState } from "react";
 import {signIn,signOut,useSession,getProviders} from 'next-auth/react';
 
 const Nav = () => {
-    const isUserLoggedIn=true;
+    const {data:session}=useSession();
 
     const [providers,setProviders]=useState(null);
     const [toggleDropdown,setToggleDropdown]=useState(false);
 
     useEffect(() => {
-        const setProviders=async () => {
+        const setUpProviders=async () => {
             const response=await getProviders();
             setProviders(response);
         }
-        setProviders();
+        setUpProviders();
     },[])
 
   return (
@@ -34,7 +34,7 @@ const Nav = () => {
 
         {/*desktop nav*/}
         <div className="sm:flex hidden">
-            {isUserLoggedIn ? (
+            {session?.user ? (
                 <div className="flex gap-3 md:gap-5">
                     <Link href='/create-prompt' className="black_btn">
                         Create Post
@@ -73,7 +73,7 @@ const Nav = () => {
 
         {/*mobile nav*/}
         <div className="sm:hidden flex relative">
-            {isUserLoggedIn ? (
+            {session?.user ? (
                 <div className="flex">
                     <Image 
                     src='assets/images/logo.svg' 
