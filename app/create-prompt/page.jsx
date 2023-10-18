21df5d4b31803c6ca184f669adeaf5992f79e960
JayadevArun <jayadevarun03@gmail.com>
2023-10-18 21:54:10 +0530

    completed create-post functioning

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -5,8 +5,11 @@ import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
 
 import Form from '@components/Form';
+import { Router } from 'next/router';
 
 const CreatePrompt = () => {
+    const router=useRouter();
+    const {data:session }=useSession();
 
     const [submitting,setSubmitting]=useState(false);
     const [post,setPost]=useState({
@@ -15,7 +18,28 @@ const CreatePrompt = () => {
     })
 
     const createPrompt=async (e)=>{
+        e.preventDefault();
+        setSubmitting(true);
 
+        try{
+            const response=await fetch('api/prompt/new',{
+                method:'POST',
+                body:JSON.stringify({
+                    prompt:post.prompt,
+                    tag:post.tag,
+                    userId:session?.user.id
+                })
+            })
+            if(response.ok){
+                router.push('/');
+            }
+        }
+        catch(error){
+            console.log(error);
+        }
+        finally{
+            setSubmitting(false); 
+        }
     }
 
     return (
