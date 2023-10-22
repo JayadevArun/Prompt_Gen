a894428169ce2639f93149bc7f6475e0516160c3
JayadevArun <jayadevarun03@gmail.com>
2023-10-22 23:42:17 +0530

    get,patch,delete update for profile edit

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -7,13 +7,14 @@ import { useRouter } from "next/navigation"
 import Profile from "@components/Profile"
 
 const MyProfile = () => {
+    const router=useRouter();
     const {data:session}=useSession();
 
     const [posts,setPosts]=useState([]);
 
     useEffect(()=>{
         const fetchPosts=async ()=>{
-          const response=await fetch(`/api/users/${session?.user?.id}/posts`);
+          const response=await fetch(`/api/users/${session?.user.id}/posts`);
           const data=await response.json();
     
           setPosts(data);
@@ -22,13 +23,27 @@ const MyProfile = () => {
         if(session?.user.id){
             fetchPosts();
         }
-      },[])
+      },[session?.user.id])
 
-    const handleEdit=async ()=>{
-
+    const handleEdit=async (post)=>{
+        router.push(`/update-prompt?id=${post._id}`)
     }
-    const handleDelete=async ()=>{
-        
+    const handleDelete=async (post)=>{
+        const hasConfirmed=confirm("Are you sure you want to delete this prompt?")
+
+        if(hasConfirmed){
+            try{
+                await fetch(`/api/prompt/${post._id.toString()}`,{
+                    method:'DELETE'
+                })
+
+                const filteredPosts=posts.filter((p)=>p._id!==post._id);
+                setPosts(filteredPosts);
+            }
+            catch(error){
+                console.log(error);
+            }
+        }
     }
 
   return (
